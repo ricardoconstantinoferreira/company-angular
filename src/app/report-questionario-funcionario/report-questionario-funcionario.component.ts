51cb76f83c087bc122044ac8144290f4a60cf043
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2025-06-11 20:53:06 -0300

    create report question by colaborator module

---
+++ src/app/report-questionario-funcionario/report-questionario-funcionario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-questionario-funcionario',
+  imports: [],
+  templateUrl: './report-questionario-funcionario.component.html',
+  styleUrl: './report-questionario-funcionario.component.scss'
+})
+export class ReportQuestionarioFuncionarioComponent {
+
+}
